b4f7e42e605cba584b0f74f3b2e7793ee41ec8ad
jpachecorincon7-rgb <jpachecorincon7@gmail.com>
2026-08-10 15:56:06 -0500

    Update prestamos.py

--- Proyecto Evaluativo de Python/GestionHerramientasDeBarrio/modulos/prestamos.py
+++ Proyecto Evaluativo de Python/GestionHerramientasDeBarrio/modulos/prestamos.py
@@ -9,26 +9,25 @@ def solicitar_prestamo(id_usuario, id_herramienta, cantidad_solicitada, fecha_fi
     import modulos.herramientas as m_herr
     herramientas = m_herr.listar_herramientas()
     
-    # 1. Validación de existencia de la herramienta
+ 
     if id_herramienta not in herramientas:
         return False, "La herramienta solicitada no existe."
     
     herr = herramientas[id_herramienta]
     
-    # Requerimiento: Estado operativo
+   
     if herr["estado"] != "activa":
         return False, f"La herramienta no está disponible. Estado actual: {herr['estado']}"
         
-    # Requerimiento: Stock disponible
+   
     if int(cantidad_solicitada) > herr["cantidad"]:
         # Requerimiento de la junta: Registrar fallas de stock en el archivo de logs
         registrar_log(f"STOCK INSUFICIENTE: Vecino {id_usuario} pidió {cantidad_solicitada} de {id_herramienta}. Stock: {herr['cantidad']}", es_error=True)
         return False, f"No hay suficientes unidades. Stock actual: {herr['cantidad']}."
         
-    # Generamos un ID correlativo simple para el diccionario (P1, P2, P3...)
+   
     id_p = f"P{len(prestamos) + 1}"
     
-    # Estructura exacta de llaves que tu compañero programó en el menú principal
     prestamos[id_p] = {
         "id": id_p,
         "usuario": id_usuario,          
@@ -56,11 +55,11 @@ def aprobar_prestamo(id_p):
     herramientas = m_herr.listar_herramientas()
     herr = herramientas[p["herramienta"]]
     
-    # Modificación del stock directo usando la función de tu compañero
+  
     nuevo_stock = herr["cantidad"] - p["cantidad"]
     m_herr.actualizar_herramienta(p["herramienta"], {"cantidad": nuevo_stock})
     
-    # Cambiamos el estado de la solicitud a activo
+   
     p["estado"] = "activo"
     
     gd(ARCH_PRESTAMOS, prestamos)
@@ -79,11 +78,9 @@ def devolver_herramienta(id_p, observaciones):
     herramientas = m_herr.listar_herramientas()
     herr = herramientas[p["herramienta"]]
     
-    # Restauramos las existencias usando la función de actualización de tu compañero
     nuevo_stock = herr["cantidad"] + p["cantidad"]
     m_herr.actualizar_herramienta(p["herramienta"], {"cantidad": nuevo_stock})
     
-    # Guardamos los cambios del recibo de préstamo
     p["estado"] = "devuelto"
     p["observaciones"] = observaciones
     
@@ -91,8 +88,6 @@ def devolver_herramienta(id_p, observaciones):
     registrar_log(f"Herramienta devuelta para el préstamo ID {id_p}. Notas: {observaciones}")
     return True, "Devolución registrada con éxito. Inventario de la junta restaurado."
 
-# --- PUENTE DE INTERFAZ OBLIGATORIO ---
-# Evita que main.py se caiga cuando llama a m_pres.cargar_datos en la línea 37
 def cargar_datos(archivo, por_defecto):
     return cd(archivo, por_defecto)
 
